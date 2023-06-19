87c705bf52657791e5b49d53a42211a379a13554
emanuelecorona <emanuele.corona@overit.it>
2023-06-19 17:38:17 +0200

    Initial Commit

---
+++ src/main/java/constants/TestScriptConst.java
@@ -0,0 +1,15 @@
+package constants;
+
+public class TestScriptConst {
+
+    public static final String CREATE_TABLE = "";
+    public static final String ADD_COLUMN = "{ALTER_ADD_COLUMN nome_tabella nome_campo {tipo_dato}}\n/";
+
+
+    public static class FieldType {
+        public static final String NUMBER = "NUMBER";
+        public static final String VARCHAR = "VARCHAR";
+        public static final String CHAR = "CHAR";
+        public static final String TIMESTAMP = "TIMESTAMP";
+    }
+}
