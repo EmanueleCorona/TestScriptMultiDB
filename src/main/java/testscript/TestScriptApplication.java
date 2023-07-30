ea8b78efe986ad332b3bd60071aece3aa9925cc5
emanuelecorona <emanuele.corona@overit.it>
2023-07-30 20:49:30 +0200

    Implementazione funzionalità “Alterazione Tipo Campo”

--- src/main/java/testscript/TestScriptApplication.java
+++ src/main/java/testscript/TestScriptApplication.java
@@ -1,6 +1,7 @@
 package testscript;
 
 import testscript.script.AddColumnScript;
+import testscript.script.AlterTypeScript;
 import testscript.script.CreateTableScript;
 
 import java.util.Scanner;
@@ -9,6 +10,7 @@ public class TestScriptApplication {
     private static final int EXIT = 0;
     private static final int TABLE_CREATION = 1;
     private static final int ADD_COLUMN = 2;
+    private static final int ALTER_COLUMN = 3;
     private static final Scanner scanner = new Scanner(System.in);
 
 
@@ -31,6 +33,10 @@ public class TestScriptApplication {
                     addColumns();
                     break;
                 }
+                case ALTER_COLUMN: {
+                    alterType();
+                    break;
+                }
             }
         }
     }
@@ -40,7 +46,8 @@ public class TestScriptApplication {
         System.out.println("0 ESCI");
         System.out.println("1 CREAZIONE TABELLA");
         System.out.println("2 AGGIUNTA COLONNE");
-        System.out.print("Scegliere un'opzione: ");
+        System.out.println("3 ALTERAZIONE TIPO CAMPO");
+        System.out.print("\nScegliere un'opzione: ");
         return getChoiceUntilIsValid();
     }
 
@@ -66,6 +73,17 @@ public class TestScriptApplication {
         createTableScript.generateStatement();
     }
 
+    private static void alterType() {
+        System.out.println("\n========== Alterazione Tipo Campo ==========");
+        System.out.print("Inserisci il nome della tabella: ");
+
+        String tableName = scanner.next().toUpperCase();
+
+        AlterTypeScript alterTypeScript = new AlterTypeScript();
+        alterTypeScript.setTableName(tableName);
+        alterTypeScript.generateStatement();
+    }
+
     private static int getChoiceUntilIsValid() {
         int choice = 0;
         boolean keepGoing = true;
@@ -74,7 +92,7 @@ public class TestScriptApplication {
             if (scanner.hasNextInt()) {
                 choice = scanner.nextInt();
 
-                if (choice < 0 || choice > 2) {
+                if (choice < 0 || choice > 3) {
                     System.out.print("Opzione non valida, scegliere un'opzione: ");
                     choice = scanner.nextInt();
                 }
---
+++ src/main/java/testscript/script/AlterTypeScript.java
@@ -0,0 +1,53 @@
+package testscript.script;
+
+import testscript.utils.TestScriptConst.Error;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static testscript.utils.TestScriptConst.*;
+
+public class AlterTypeScript extends ScriptGenerator {
+    private static final String ALTER_TYPE = "{ALTER_TYPE " + TABLE_NAME + " " + FIELD_NAME + " {" + FIELD_TYPE + "}}" + SCRIPT_SEPARETOR;
+
+    @Override
+    public void generateStatement() {
+        try {
+            writeHeader();
+
+            String sql = "";
+            boolean readyToWrite = false;
+
+            while ((row = reader.readLine()) != null) {
+                if (!row.isEmpty()) {
+
+                    if (isFieldName) {
+                        sql = getFormattedStatement().replace(FIELD_NAME, getFormattedFieldName(row));
+                        isFieldName = false;
+                    } else {
+                        sql = sql.replace(FIELD_TYPE, getFormattedFieldType(row));
+                        isFieldName = true;
+                        readyToWrite = true;
+                    }
+
+                    if (readyToWrite) {
+                        writer.append(sql);
+                        readyToWrite = false;
+                    }
+
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(Error.ERR_FILE_NOT_FOUND);
+        } catch (IOException e) {
+            throw new RuntimeException(Error.ERR_FILE_WRITING);
+        } finally {
+            closeResources();
+        }
+    }
+
+    protected String getFormattedStatement() {
+        return ALTER_TYPE.replace(TABLE_NAME, tableName);
+    }
+}
